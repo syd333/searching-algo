befeba8740442226cace995279502fb9a65f609b
syd333 <sydmiranda@icloud.com>
2021-07-11 20:02:19 -0400

    linear search

---
+++ linearSearch.js
@@ -0,0 +1,23 @@
+// LINEAR SEARCH PSEUDOCODE
+
+// this function accepts an array and a value
+// loop through array and check if the current array element is equal to the value
+// if it is return the index at which the element is found
+// if the value is never found, return -1
+
+function linear (arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return arr[i]
+        } 
+        return -1
+    }
+}
+
+function linearSearch(arr, val){
+
+  }
+
+// linearSearch([10, 15, 20, 25, 30], 15)
+// linearSearch([100], 100)
+// linearSearch([1, 2, 3, 4, 5], 6)
