655e03115effcb893dc5f41373b76418795d4693
syd333 <sydmiranda@icloud.com>
2021-07-13 13:07:01 -0400

    refactor

--- binarySearch.js
+++ binarySearch.js
@@ -17,7 +17,7 @@ function binarySearch(arr, val) {
   let start = 0;
   let end = arr.length - 1;
   let middle = Math.floor((start + end) / 2);
-  while (arr[middle] !== val) {
+  while (arr[middle] !== val && start <= end) {
     if (val < arr[middle]) {
       end = middle - 1;
     } else {
@@ -30,6 +30,18 @@ function binarySearch(arr, val) {
   }
   return - 1;
 }
+
+function binarySearchRefactor(arr, val) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== val && start <= end) {
+      if (val < arr[middle]) end = middle - 1; 
+      else start = middle + 1;
+      middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === val ? middle : -1;
+  }
 
 binarySearch(
   [5, 6, 10, 13, 14, 18, 30, 34, 25, 37, 40, 44, 64, 79, 84, 86],
